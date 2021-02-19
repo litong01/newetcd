24591821b3a90ac86ab202b2bbc6d651310487e0
litong01 <litong01@us.ibm.com>
2021-02-19 04:01:05 +0000

    Added logging info
    Signed-off-by: litong01 <litong01@us.ibm.com>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"log"
 
 	"github.com/gorilla/mux"
 )
@@ -18,10 +19,18 @@ func main() {
 	cert := os.Getenv("TLS_CERT")
 	key := os.Getenv("TLS_KEY")
 
+	var err error
 	if len(cert) > 0 && len(key) > 0 {
-		http.ListenAndServeTLS(":8443", cert, key, r)	
+		log.Println("TLS enabled")
+		log.Printf("The cert is at %s", cert)
+		log.Printf("The key is at %s", key)
+		err = http.ListenAndServeTLS(":8080", cert, key, r)	
 	} else {
-		http.ListenAndServe(":8080", r)		
+		log.Println("TLS disabled")
+		err = http.ListenAndServe(":8080", r)
 	}
 
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
