e94f3138345d737edab905d95e4b15feeab11cbb
litong01 <litong01@us.ibm.com>
2021-02-19 14:44:50 +0000

    Added support for more tls version and almost all possible cipherSuite
    This commit added support for
    tls version1.0 to 1.3
    curveP521, curveP384, curveP256
    nearly all cipherSuite
    
    Signed-off-by: litong01 <litong01@us.ibm.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"net/http"
 	"os"
 	"log"
@@ -24,7 +25,55 @@ func main() {
 		log.Println("TLS enabled")
 		log.Printf("The cert is at %s", cert)
 		log.Printf("The key is at %s", key)
-		err = http.ListenAndServeTLS(":8080", cert, key, r)	
+
+		cfg := &tls.Config{
+			MinVersion:               tls.VersionTLS10,
+			MaxVersion:				  tls.VersionTLS13,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				// TLS 1.0 - 1.2 chipher suites
+				tls.TLS_RSA_WITH_RC4_128_SHA,
+				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				// tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				// tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+
+				// TLS 1.3 cipher suites.
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+
+				// TLS_FALLBACK_SCSV isn't a standard cipher suite but an indicator
+				// that the client is doing version fallback. See RFC 7507.
+				tls.TLS_FALLBACK_SCSV,
+			},
+		}
+		srv := &http.Server{
+			Addr:         ":8080",
+			Handler:      r,
+			TLSConfig:    cfg,
+			TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+		}
+
+		err = srv.ListenAndServeTLS(cert, key)
 	} else {
 		log.Println("TLS disabled")
 		err = http.ListenAndServe(":8080", r)
